f6176367fec3fb7c36bf3aa52bce573e29de7cdd
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 21:57:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -35,6 +35,16 @@ def nyc_pigeon_organizer(data)
     :color => ["white", "brown"],
     :gender => ["male"],
     :lives => ["Central Park"]
+  },
+  "Luca" => {
+    :color => ["brown"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lola" => {
+    :color => ["black"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
   }
 }
 end
