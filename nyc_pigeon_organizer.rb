34cadabd345690eaa63db0fa2271ab2f95db03c4
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 22:12:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,41 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {
+  "Luca" => {
+    :color => ["brown"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lola" => {
+    :color => ["black"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Theo, Peter Jr." => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway", "Library"]
+  },
+  "Lucky" => {
+    :color => ["purple"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  },
+  "Ms. K" => {
+    :color => ["grey", "white"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Queenie, Alex" => {
+    :color => ["white", "brown"],
+    :gender => ["female"], ["male"],
+    :lives => ["Subway"], ["Central Park"]
+  },
+  "Andrew" => {
+    :color => ["white"],
+    :gender => ["male"],
+    :lives => ["City Hall"]
+  },
+  
+}
 end
